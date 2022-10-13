49b51c82cec94e34fe3748a77e48dcec2d5e24e4
rotten <1132264073@qq.com>
2022-10-13 19:59:51 +0800

    初始化

---
+++ tksp/src/test/java/com/tk/tksp/TkspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tk.tksp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TkspApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
